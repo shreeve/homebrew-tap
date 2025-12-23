033d32ef3c22ecce05d0c031caf95176436b7b1c
Steve Shreeve <steve.shreeve@gmail.com>
2025-12-23 12:38:22 -0700

    Update pairsync to 0.6.1

--- Casks/pairsync.rb
+++ Casks/pairsync.rb
@@ -1,6 +1,6 @@
 cask "pairsync" do
-  version "0.6.0"
-  sha256 "54c5e45b65a845f99d32c9ed00a32d4632482d405ec3c196a25753e0ddbc4122"
+  version "0.6.1"
+  sha256 "a0f2fde3f9236aae0dd843034b3f6ee8ab2b770d4d6d7e42578c2467e46a6277"
 
   url "https://github.com/shreeve/pairsync/releases/download/v#{version}/PairSync-#{version}.dmg"
   name "PairSync"
