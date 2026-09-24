165676bd240d6a4e7721db9c881a7764f6c246a2
Steve Shreeve <steve.shreeve@gmail.com>
2026-09-24 15:53:27 -0600

    Update git-enc to 0.1.1

--- Formula/git-enc.rb
+++ Formula/git-enc.rb
@@ -10,23 +10,23 @@ class GitEnc < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/shreeve/git-enc/releases/download/v0.1.0/git-enc-v0.1.0-osx-arm64.tar.gz"
-      sha256 "6d7db9a6221b27c0f76736512c5a8c21d1117a318bac765c666e42c9e50c4418"
+      url "https://github.com/shreeve/git-enc/releases/download/v0.1.1/git-enc-v0.1.1-osx-arm64.tar.gz"
+      sha256 "9fb8fa48986ad096726fe71cf29d041957874028b66d3f312350fab9319b6cc7"
     end
     on_intel do
-      url "https://github.com/shreeve/git-enc/releases/download/v0.1.0/git-enc-v0.1.0-osx-amd64.tar.gz"
-      sha256 "fe81ee661511eac99b9d28c2f67efcdd4f391a47eaeb3b1e97c34308a0487129"
+      url "https://github.com/shreeve/git-enc/releases/download/v0.1.1/git-enc-v0.1.1-osx-amd64.tar.gz"
+      sha256 "73e0eb168d8e8e4f7f88363587cd07b784a4f2c89252f31c51101aa76911c634"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shreeve/git-enc/releases/download/v0.1.0/git-enc-v0.1.0-linux-arm64.tar.gz"
-      sha256 "ffb1fa46fc098821caf05ed70fd782b28fe252cd41cfd37e4eeeba96766e0bdf"
+      url "https://github.com/shreeve/git-enc/releases/download/v0.1.1/git-enc-v0.1.1-linux-arm64.tar.gz"
+      sha256 "0a9836a1ea845123235118c7e9969e074c9f1a8569e7fc467fdf9f795532497c"
     end
     on_intel do
-      url "https://github.com/shreeve/git-enc/releases/download/v0.1.0/git-enc-v0.1.0-linux-amd64.tar.gz"
-      sha256 "d8105e0ee4c4000c48520f4f6c32742929efe6a9a10c3b39b6214797549c413e"
+      url "https://github.com/shreeve/git-enc/releases/download/v0.1.1/git-enc-v0.1.1-linux-amd64.tar.gz"
+      sha256 "1f76c3a95c67ec50b6652af26da006e13d5382b2a388d30c71b8c41181e2df0c"
     end
   end
 
